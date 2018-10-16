e4e3ecc31520a8c4c6babb9b66d255f43c79c236
Areeb Yaqub <areeb.yaqub@sjsu.edu>
2018-10-16 00:46:36 -0700

    4.8

---
+++ 4.8/FilterTester.java
@@ -0,0 +1,13 @@
+package filter;
+
+public class FilterTester {
+	public static void main(String[] args)
+    {
+		String[] words = {"amy", "cry", "why", "hi", "Areeb", "Kuristina", "Angus", "dolt" };
+		
+		Filterer threeLetters = new Filterer();
+		threeLetters.filter(words,  )
+		
+    }
+    
+}
