91f7b4bfa38600472817a240c30cc055f42e0bb8
Areeb Yaqub <areeb.yaqub@sjsu.edu>
2018-10-17 21:10:16 -0700

    arrays instead of arraylist

--- 4.10/RectangleTester.java
+++ 4.10/RectangleTester.java
@@ -2,25 +2,26 @@ package rectangle;
 
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class RectangleTester {
 
 	public static void main(String[] args)
 	{
-		ArrayList<Rectangle2D.Double> rect = new ArrayList<>();
-		rect.add(new Rectangle2D.Double(123, 321, 300, 300));
-		rect.add(new Rectangle2D.Double(25, 50, 20, 20));
-		rect.add(new Rectangle2D.Double(300, 300, 123, 321));
-		rect.add(new Rectangle2D.Double(3, 3, 3, 3));
-		rect.add(new Rectangle2D.Double(123, 97, 60, 30));
-		rect.add(new Rectangle2D.Double(123, 321, 300, 300));
-		rect.add(new Rectangle2D.Double(69, 96, 100, 100));
-		rect.add(new Rectangle2D.Double(123, 321, 200, 100));
+		Rectangle2D.Double[] rect = {
+		         new Rectangle2D.Double(123, 321, 300, 300),
+		new Rectangle2D.Double(25, 50, 20, 20),
+		new Rectangle2D.Double(300, 300, 123, 321),
+		new Rectangle2D.Double(3, 3, 3, 3),
+		new Rectangle2D.Double(123, 97, 60, 30),
+		new Rectangle2D.Double(123, 321, 300, 300),
+		new Rectangle2D.Double(69, 96, 100, 100),
+		new Rectangle2D.Double(123, 321, 200, 100)};
 		
 		
 		RectangleComparator comp = new RectangleComparator();
-		Collections.sort(rect, comp);
+		Arrays.sort(rect, comp);
 		for(Rectangle2D.Double r: rect)
 		{
 			System.out.println(r.getX()+" "+r.getY()+" "+r.getWidth()+" "+r.getHeight());
